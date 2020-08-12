8806d038646345df17a6b547d7677e9b2268d956
SimonEnsemble <cory.simon@oregonstate.edu>
2020-08-11 22:32:59 -0700

    add time to peak infection

--- sir_sim.jl
+++ sir_sim.jl
@@ -42,6 +42,9 @@ md"## peak prevalence, final size as a function of $\mathcal{R}_0$"
 # ╔═╡ 3dd997b8-db97-11ea-3fdf-33ba59dcdce6
 md"## herd immunity"
 
+# ╔═╡ d54ba48e-dc56-11ea-0ac4-8d213422f65f
+md"### find time to peak infection"
+
 # ╔═╡ 988e2cdc-db85-11ea-006e-398146591b74
 begin
 	using DifferentialEquations, PyPlot, Roots, Printf
@@ -61,6 +64,8 @@ function update_f!(f, u, p, t)
     s = u[1]
     i = u[2]
     r = u[3]
+	# for clarity, upack p[1] as R₀
+	R₀ = p[1]
     # update f
     f[1] = -R₀ * s * i
     f[2] = i * (R₀ * s - 1.0)
@@ -77,7 +82,7 @@ end
 begin
 	# define the ODE problem
 	time_span = (0.0, 25.0)
-	prob = ODEProblem(update_f!, u0, time_span)
+	prob = ODEProblem(update_f!, u0, time_span, [R₀])
 	
 	# numerically solve ODE
 	sol = solve(prob) # now sol(10.0) gives numerical approx. to sol'n at t=10
@@ -119,9 +124,11 @@ begin
 	ylim([-1e-3, 1+1e-3])
 	
 	# info
-	bbox_props = Dict(:boxstyle=>"round", :fc=>"0.95", :ec=>"0.5")#, :alpha=0.9)
-	sim_settings = L"$\mathcal{R}_0=2$" * "\n" * L"$[$I$](0)=10^{-5}$"
+	bbox_props = Dict(:boxstyle=>"round", :fc=>(1.0, 0.98, 0.98), :ec=>"0.5")
+		#:ec=>"0.5")#, :alpha=0.9)
+	sim_settings = L"$\mathcal{R}_0=2$" * "\n" * L"$[$I$]_0=10^{-5}$"
 	@assert u0[2] ≈ 10^(-5)
+	@assert R₀ ≈ 2.0
 	text(2.5, 0.5, 
 		sim_settings, bbox=bbox_props,
 	    va="center")
@@ -167,7 +174,7 @@ begin
 	
 	t_max = 15.0
 	
-	figure()
+	figure(figsize=(8, 7))
 	plot(s, i, color="C4")
 	for k = 1:length(s)-1
 	    plot(s[k:k+1], -s[k:k+1] .+ log.(s[k:k+1]) / R₀ .+ 1, 
@@ -180,24 +187,53 @@ begin
 		plot(s, 1 .- s .+ 1 / R₀ * log.(s / u0[1]))
 	end
 	
-	# initial conditions
-	plot([1], [0], marker="o", markersize=7, clip_on=false, 
-	    color="k", label=L"$([$S$]_0, [$I$]_0)$", linestyle="None")
-	# peak prevalence
-	plot([1 / R₀], [1 - 1 / R₀ .* (1 + log(R₀ * u0[1]))], marker="s", 
-	    markersize=7, clip_on=false, color="k",
-		label=L"$(\mathcal{R}_0^{-1}, \max_t [$I$](t))$", linestyle="None")
-	# final conditions
-	plot([s∞], [0.0], marker="X", markersize=7, clip_on=false,
-	    color="k", label=L"$([$S$]_\infty, 0)$", linestyle="None")
+	# initial conditions, peak prevalence, and final conditions
+	xys = [(u0[1],  u0[2]), 
+		   (1 / R₀, 1 - 1 / R₀ .* (1 + log(R₀ * u0[1]))),
+		   (s∞, 0.0)
+	]
+	labels = [L"$([$S$]_0, [$I$]_0)$", 
+			  L"$(\mathcal{R}_0^{-1}, \max_t [$I$](t))$",
+		      L"$([$S$]_\infty, 0)$"
+	]
+	dxs = [-0.14, -0.0, -0.1]
+	dys = [0.175, 0.15, 0.15]
+	legend_not_annotate = false
+	for k = 1:3
+		plot(xys[k][1], xys[k][2], marker="X", markersize=7, clip_on=false, 
+	    	color="k", label=labels[k], linestyle="None")
+		if ! legend_not_annotate
+			annotate(labels[k],
+				xy=xys[k], xycoords="data",
+				xytext=(xys[k][1] + dxs[k], xys[k][2] + dys[k]), textcoords="data",
+				arrowprops=Dict(:arrowstyle => "->", #linestyle="dashed",
+								:color => "k"
+								),
+				ha="center",
+				bbox=Dict(:boxstyle=>"round", :fc=>(1.0, 0.98, 0.98), :ec=>"0.5",
+					:alpha=>0.8),
+				zorder=1241
+			 )
+		end
+	end
 	
 	xticks([i * 0.25 for i = 0:5])
 	yticks([i * 0.25 for i = 0:5])
+	
+	if legend_not_annotate
+		text(0.65, 0.5, 
+			sim_settings, #bbox=bbox_props,
+			va="center")
+	else
+		text(0.65, 0.85, 
+			sim_settings, #bbox=bbox_props,
+			va="center")
+	end
+	
+	if legend_not_annotate
+		legend(loc="upper right", numpoints=1, prop=Dict(:size => 14))
+	end
 
-	text(0.65, 0.5, 
-		sim_settings, #bbox=bbox_props,
-		va="center")
-	legend(loc="upper right", numpoints=1, prop=Dict(:size => 14))
 	
 	# color invalid region
 	fill_between([0.0, 1.0], [1, 0], [1, 1], color="0.8")
@@ -209,8 +245,8 @@ begin
 	
 	# set equal axes
 	plt.gca().set_aspect("equal")
-	xlim([-1e-3, 1+1e-3])
-	ylim([-1e-3, 1+1e-3])
+	xlim([-1e-2, 1])
+	ylim([-1e-2, 1])
 	
 	tight_layout()
 	savefig("phase_plane.pdf", format="pdf", bbox_inches="tight")
@@ -220,10 +256,10 @@ end
 # ╔═╡ fb3e3094-db93-11ea-2ea0-ad854ade3702
 begin
 	figure()
-	plot(t, i, color="C2", clip_on=false)#, label=L"$[I](t)$")
+	plot(t, i, color=sir_to_color["I"], clip_on=false)#, label=L"$[I](t)$")
 	plot(t[t .< 11], exp.(t[t .< 11] * (R₀ - 1)) * u0[2], 
 		linestyle="--", color="gray")
-	text(x_pos, i[end] + dy/3, L"$[$I$](t)$", color="C2")
+	text(x_pos, i[end] + dy/3, L"$[$I$](t)$", color=sir_to_color["I"])
 	xlim([0, time_span[2]])
 	xlabel(L"non-dimensional time, $\gamma t$")
 	ylabel(L"$[$I$](t)$")
@@ -240,10 +276,10 @@ end
 # ╔═╡ 9b8aba12-db95-11ea-0680-3b2a75a4af3b
 begin
 	figure()
-	plot(t, i, color="C2")#, label=L"$[I](t)$")
+	plot(t, i, color=sir_to_color["I"])#, label=L"$[I](t)$")
 	plot(t[t .< 11], exp.(t[t .< 11] * (R₀ - 1)) * u0[2], 
 		linestyle="--", color="gray")
-	text(x_pos, i[end] + dy/20, L"$[$I$](t)$", color="C2")
+	text(x_pos, i[end] + dy/20, L"$[$I$](t)$", color=sir_to_color["I"])
 	xlim([-1e-3, time_span[2]+1e-2])
 	yscale("log")
 	xlabel(L"non-dimensional time, $\gamma t$")
@@ -261,7 +297,7 @@ end
 # ╔═╡ 3fab154c-db96-11ea-0ff6-d5252766c2b3
 begin
 	R₀s = range(1.0, 5.0, length=100)
-	figsize = (5, 4.5)
+	figsize = (4.5, 4.0) # used for three plots below.
 end
 
 # ╔═╡ 4eb12950-db96-11ea-0797-39eb74fe7087
@@ -303,7 +339,7 @@ begin
 	figure(figsize=figsize)
 	plot(R₀s, 1.0 .- s∞s, color="C1", clip_on=false)
 	xlabel(L"$\mathcal{R}_0$")
-	ylabel(L"$1-[$S$]_\infty=[$R$]_\infty$")
+	ylabel(L"$[$R$]_\infty=1-[$S$]_\infty$")
 	xlim([1-1e-3, 5+1e-3])
 	ylim([-1e-3, 1+1e-3])
 	title("final size")
@@ -324,7 +360,7 @@ begin
 	fill_between(R₀s, v, ones(length(v)), color="C6", alpha=0.4, 
 		label=L"$v>1-\mathcal{R}_0^{-1}$")
 	plot(R₀s, v, color="C6", clip_on=false)
-	plot(R₀s, 1 .- s∞s, color="C1", label=L"$1-[$S$]_\infty=[$R$]_\infty$",
+	plot(R₀s, 1 .- s∞s, color="C1", label=L"$[$R$]_\infty=1-[$S$]_\infty$",
 		clip_on=false)
 	xlabel(L"$\mathcal{R}_0$")
 	ylabel("fraction of population")
@@ -334,6 +370,51 @@ begin
 	title("herd immunity")
 	tight_layout()
 	savefig("herd_immunity.pdf", format="pdf")
+	gcf()
+end
+
+# ╔═╡ e0ec7372-dc56-11ea-007e-b9e622a0af95
+begin
+	# define the ODE problem
+	new_time_span = (0.0, 65.0)
+	
+	R₀s_new = range(1.17, 5.0, length=150)
+	time_to_peak = similar(R₀s_new)
+	
+	for (i, R₀) in enumerate(R₀s_new)
+		prob = ODEProblem(update_f!, u0, new_time_span, [R₀])
+		# numerically solve ODE
+		sol = solve(prob)
+	
+		# find time at peak by grid search
+		ts = range(0.0, R₀ < 2 ? new_time_span[2] : 15.0, length=500) # t-grid
+		new_i = [sol.(t_j)[2] for t_j in ts]
+		time_to_peak[i] = ts[argmax(new_i)]
+		
+		# to make sure we are finding the time to peak, get max I(t)
+		Imax = 1 - 1 / R₀ .* (1 + log(R₀ * u0[1]))
+		if i != 1
+			continue
+		end
+		@assert maximum(new_i) <= Imax
+		@assert maximum(new_i) >= 0.999 * Imax
+	end
+	
+	ymax = 50.0
+	figure()
+	ylabel(L"$\alpha$" * " argmax " * L"$[$I$](t)$")
+	xlabel(L"$\mathcal{R}_0$")
+	plot(R₀s_new, time_to_peak, color="C7")
+	title("time to peak prevalence")
+	xlim([-1e-3, 5+1e-3])
+	ylim([-1e-1, ymax])
+	hlines(0.0, 0.0, 1.0, clip_on=false, color="C7")
+	vlines(1.0, 0.0, ymax, linestyle="--", color="C6", linewidth=2)
+	text(4.0, 30.0, 
+		LaTeXString(split(sim_settings)[2]), bbox=bbox_props,
+		va="center", ha="center")
+	tight_layout()
+	savefig("time_to_first_peak.pdf", format="pdf")
 	gcf()
 end
 
@@ -363,3 +444,5 @@ end
 # ╠═7e6e40b0-db96-11ea-195c-837936eaec5a
 # ╟─3dd997b8-db97-11ea-3fdf-33ba59dcdce6
 # ╠═43b2803c-db97-11ea-0b9c-7f5d0fdff33d
+# ╟─d54ba48e-dc56-11ea-0ac4-8d213422f65f
+# ╠═e0ec7372-dc56-11ea-007e-b9e622a0af95
